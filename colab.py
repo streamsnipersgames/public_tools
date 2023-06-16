1f46b45db16f34d3e64911ddcd31bba246fa742d
streamsnipersgames <streamsnipersgames@gmail.com>
2023-06-16 16:21:21 -0400

    mount drive in loader

--- colab.py
+++ colab.py
@@ -19,8 +19,11 @@ def write_aws_credentials(access_key, secret_key, region):
 
 
 def load_aws_credentials():
+    from google.colab import drive
+    drive.mount('/content/drive')
+    
     import os
     # !export AWS_SHARED_CREDENTIALS_FILE=/content/drive/MyDrive/config/awscli.ini
     path = "/content/drive/My Drive/config/awscli.ini"
     os.environ['AWS_SHARED_CREDENTIALS_FILE'] = path
-    print(os.environ['AWS_SHARED_CREDENTIALS_FILE'])
+    print(f"loaded AWS credentials file as: " + os.environ['AWS_SHARED_CREDENTIALS_FILE'])
